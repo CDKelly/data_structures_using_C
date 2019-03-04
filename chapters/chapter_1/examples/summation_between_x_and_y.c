9ed2859e432266d72de213ed3244f34e3af7af16
Connor Kelly <connor.david.kelly@gmail.com>
2019-03-04 10:25:51 -0500

    summation between two nos. example

---
+++ chapters/chapter_1/examples/summation_between_x_and_y.c
@@ -0,0 +1,20 @@
+// example 5, pg. 23
+
+// Calculate the sum of number from x to y
+
+#include <stdio.h>
+int main() {
+	int x, y, i, sum = 0;
+	printf("\nEnter a number x : \t");
+	scanf("%d", &x);
+	i=x;
+	printf("\nEnter a number y : \t");
+	scanf("%d", &y);
+
+	while(i<=y) {
+	sum += i;
+		i++;
+	}
+	printf("\nThe sum of numbers from %d to %d = %d\n", x, y, sum);
+	return 0;
+}
