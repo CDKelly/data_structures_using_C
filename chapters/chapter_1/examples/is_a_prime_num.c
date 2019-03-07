413affa3657bc6e38185bdc7479fa0198175ef1c
Connor Kelly <connor.david.kelly@gmail.com>
2019-03-07 13:06:40 -0500

    added chapter 1 programming example 7

---
+++ chapters/chapter_1/examples/is_a_prime_num.c
@@ -0,0 +1,26 @@
+// example 7, pg. 26
+
+// Determine whether a given number is a prime or a composite number
+
+#include <stdio.h>
+int main() {
+	int is_prime = 0, i, num;
+
+	printf("\nEnter a positive number : \t");
+	scanf("%d", &num);
+
+	for(i=2; i < num/2; i++) {
+		if (num%i == 0) {
+			is_prime = 1;
+			break;
+		}
+	}
+
+	if (is_prime == 1) {
+		printf("\n%d is a composite number", num);
+	} else {
+		printf("\n%d is a prime number", num);
+	}
+
+	return 0;
+}
