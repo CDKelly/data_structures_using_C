a13409470abce11edae76aab3ca2ff2151ae504c
Connor Kelly <connor.david.kelly@gmail.com>
2019-03-15 21:21:35 -0400

    testing different code

--- chapters/chapter_1/exercises/count_vowels.c
+++ chapters/chapter_1/exercises/count_vowels.c
@@ -4,13 +4,13 @@
 
 #include <stdio.h>
 #include <regex.h>
-
+#include <string.h>
 
 /* return 1 for match, 0 for no match or errors */
 int match(const char *string, const char *pattern) {
 	regex_t re;
 
-	if (regcomp(&re, pattern, REG_EXTENDED|REGNOSUB) != 0) {
+	if (regcomp(&re, pattern, REG_EXTENDED|REG_NOSUB|REG_ICASE) != 0) {
 		return 0; // check for compilation error
 	}
 
@@ -22,4 +22,32 @@ int match(const char *string, const char *pattern) {
 	}
 
 	return 1;
+}
+
+int main() {
+	char user_str[60];
+	int count;
+	const char *pattern = "[aeiou]";
+
+	printf("\nEnter a string (less than 60 characters) : \t");
+	scanf("%s", user_str);
+
+	int str_len = strlen(user_str);
+	const char *user_char = user_str;
+
+	printf("\ndoes match: %d\n", match(user_char, pattern));
+
+	if (strchr(user_char, 'a')) {
+		printf("\nstrchr line reached");
+	}
+
+	for(int i=0; i<str_len; i++) {
+		char curr_char = user_str[i];
+		printf("\ncurr char %c\n", user_str[i]);
+		printf("\npattern pointer: %c\n", pattern[i]);
+		if (match(&curr_char, pattern)) {
+			printf("\nthis line reached\n");
+			printf("\n=======\n");
+		}
+	}
 }
