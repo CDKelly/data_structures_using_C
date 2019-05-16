33ea276bf0b0b3849da7d36d5ada448d0e749fa6
Connor Kelly <connor.david.kelly@gmail.com>
2019-05-16 13:14:38 -0400

    WIP: properly insert values into multi-dim arr

---
+++ chapters/chapter_3/examples/multi_dim_arr_salesmen.c
@@ -0,0 +1,55 @@
+// example 18, pgs. 97-98
+
+/* Write a program that takes in a number of salesmen and products to sell.
+   The program should print out the total sales by each salesman and the
+   total sales by each product.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int get_num_of_salesmen();
+int get_num_of_prods();
+int* read_arr(int s, int p);
+
+
+int main() {
+
+	int num_of_salesmen = get_num_of_salesmen();
+	int num_of_prods = get_num_of_prods();
+
+	int *arr = read_arr(num_of_salesmen, num_of_prods);
+
+
+}
+
+int get_num_of_salesmen() {
+	int num_of_salesmen;
+
+	printf("\nEnter the number of salesmen in your company : ");
+	scanf("%d", &num_of_salesmen);
+
+	return num_of_salesmen;
+}
+
+int get_num_of_prods() {
+	int num_of_prods;
+
+	printf("\nEnter the number of products each salesmen must sell : ");
+	scanf("%d", &num_of_prods);
+
+	return num_of_prods;
+}
+
+int* read_arr(int num_of_salesmen, int num_of_prods) {
+	int *arr = (int *) malloc(num_of_salesmen * num_of_prods * sizeof(int));
+
+	for(int i = 0; i < num_of_salesmen; i++) {
+		printf("\nFor each product, enter the number of products sold by salesmen %d", i+1);
+		for(int j = 0; j < num_of_prods; j++) {
+			printf("\nproduct %d : ", j+1);
+			scanf("%d", &(arr[(num_of_prods * i) + j]));
+		}
+	}
+	return 0;
+}
